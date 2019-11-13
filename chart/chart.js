6eedea50f6cd62082009a65f32a34cd05e9a4233
edwardzhong <edwardloveyou@gmail.com>
2019-11-14 01:07:50 +0800

    update

--- chart/chart.js
+++ chart/chart.js
@@ -2,8 +2,6 @@
  * canvas chart
  * author:jeff zhong
  */
-window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
-    return window.setTimeout(callback, 1000 / 60); };
 
 function roundRect(ctx, x, y, width, height, radius) {
     ctx.beginPath();
